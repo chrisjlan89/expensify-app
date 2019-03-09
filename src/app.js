2d0f0c242867b8e4e6889601fab25ee33e6e83d8
Chrisjlan89 <chrisj17@gmail.com>
2019-03-09 10:25:52 -0500

    new tests added

--- src/app.js
+++ src/app.js
@@ -8,9 +8,9 @@ import configureStore from './store/configureStore';
 import {addExpense} from './actions/expenses';
 import {setTextFilter} from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
-
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+import 'react-dates/lib/css/_datepicker.css';
 import { setTimeout } from 'timers';
 
 const store = configureStore();
--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -4,17 +4,23 @@ import filters from '../reducers/filters';
 import ExpenseListItem from './ExpenseListItem';
 import selectExpenses from '../selectors/expenses';
 
-const ExpenseList = (props) => (
+export const ExpenseList = (props) => (
     
     <div>
-        <h1>ExpenseList </h1>
+       {
+           props.expenses.length === 0 ? (
+            <p> No expenses</p>
+           ) : (
+            props.expenses.map((expense, index) => (
+                <ExpenseListItem key={expense.id} {...expense}/>
+            ))
+            
+
+           )
+       }
      
       {
-                props.expenses.map((expense, index) => (
-                    <ExpenseListItem key={index} {...expense}/>
-                ))
                 
-
                 
             
             }
